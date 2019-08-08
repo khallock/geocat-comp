1467db25c13cb1ce9297c05a6fb6a6a661c8ec83
Kevin Hallock <hallock@ucar.edu>
2019-08-08 11:56:48 -0600

    Add CircleCI config

---
+++ ci/build-ncomp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -eo pipefail
+
+conda config --set always_yes true --set changeps1 false --set quiet true
+conda install git
+conda config --add channels conda-forge
+git clone https://github.com/khallock/ncomp.git
+cd ncomp
+git checkout circleci
+echo "conda env create -f ci/environment-dev-$(uname).yml --name ${NCOMP_ENV_NAME} --quiet"
+conda env create -f ci/environment-dev-$(uname).yml --name ${NCOMP_ENV_NAME} --quiet
+conda env list
+source activate ${NCOMP_ENV_NAME}
+autoreconf --install
+./configure --prefix=${CONDA_PREFIX}
+make install
