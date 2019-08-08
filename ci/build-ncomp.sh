247c64bd9508334633f561291d851c94199dc3d6
Kevin Hallock <hallock@ucar.edu>
2019-08-08 15:09:46 -0600

    Use CircleCI dependency caching

--- ci/build-ncomp.sh
+++ ci/build-ncomp.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda install git
 conda config --add channels conda-forge
-git clone https://github.com/khallock/ncomp.git
+git clone ${NCOMP_GIT_REPO}
 cd ncomp
 git checkout circleci
 echo "conda env create -f ci/environment-dev-$(uname).yml --name ${NCOMP_ENV_NAME} --quiet"
